5ff3924c17a95cb08d791871d4ac497e25afb357
alcjamgit <alcarazjames@gmail.com>
2014-06-05 12:24:04 +0800

    Unit Test Project Using Installed Generator

---
+++ RealEstateAppTests/Controllers/RealtyAdControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealEstateApp.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace RealEstateApp.Controllers.Tests
+{
+  [TestClass()]
+  public class RealtyAdControllerTests
+  {
+    [TestMethod()]
+    public void RealtyAdControllerTest()
+    {
+      Assert.Fail();
+    }
+  }
+}
