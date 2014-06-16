6c046819aa0a0787d2805189608a5c6f469a80e7
alcjamgit <alcarazjames@gmail.com>
2014-06-16 13:48:08 +0800

    checkpoint

---
+++ OldFiles/RealEstateAppTests/DependencyInjections/MockDb.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RealEstateApp.DataAccessLayer;
+using RealEstateApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.UnitTest.DependencyInjections
+{
+  public class MockDb : IdentityDbContext, IUnitOfWork
+  {
+
+    #region IUnitOfWork Implementation
+
+    //Properties set to virtual so that mocking frameworks can override behavior
+    public virtual IGenericRepository<RealtyAd> RealtyAdRepo
+    {
+      get { return new MockGenericRepo<RealtyAd>(); }
+    }
+
+    public virtual IGenericRepository<RealtyAdImage> RealtyAdImageRepo
+    {
+      get { return new MockGenericRepo<RealtyAdImage>(); }
+    }
+
+    public virtual IGenericRepository<RealtyAdImageDefault> RealtyAdImageDefaultRepo
+    {
+      get { return new MockGenericRepo<RealtyAdImageDefault>(); }
+    }
+
+    public virtual IGenericRepository<RealtyAdMessage> RealtyAdMessageRepo
+    {
+      get { return new MockGenericRepo<RealtyAdMessage>(); }
+    }
+
+    public virtual IGenericRepository<RealtyAdPageView> RealtyAdPageViewRepo
+    {
+      get { return new MockGenericRepo<RealtyAdPageView>(); }
+    }
+
+    public virtual IGenericRepository<ApplicationUser> ApplicationUserRepo
+    {
+
+      get
+      {
+        //UserStore<ApplicationUser> test = new UserStore<ApplicationUser>();
+        List<ApplicationUser> appUser = new List<ApplicationUser>();
+        //appUser.Add(new ApplicationUser() { Id = "user1", UserName = "JohnDoe", Email = "JohnDoe@gmail.com", PasswordHash = "TopSecret1" });
+        //appUser.Add(new ApplicationUser() { Id = "user2", UserName = "JaneDoe", Email = "JaneDoe@gmail.com", PasswordHash = "TopSecret1" });
+        return new MockGenericRepo<ApplicationUser>(appUser);
+      }
+    }
+    public virtual IGenericRepository<City> CityRepo
+    {
+      get { return new MockGenericRepo<City>(); }
+    }
+
+    public void Commit()
+    {
+      //TODO
+    }
+
+
+    #endregion
+
+
+  }
+}
--- RealEstateApp/Global.asax.cs
+++ RealEstateApp/Global.asax.cs
@@ -16,6 +16,7 @@ namespace RealEstateApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
